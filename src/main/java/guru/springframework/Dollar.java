cde1a5f7170c900076386d37b4dae0dbce9ffe49
Fernando Felipe <fefernando301@hotmail.com>
2023-07-10 18:21:22 -0300

    :fire: franc-ly speaking

--- src/main/java/guru/springframework/Dollar.java
+++ src/main/java/guru/springframework/Dollar.java
@@ -2,7 +2,7 @@ package guru.springframework;
 
 public class Dollar {
 
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
@@ -10,5 +10,10 @@ public class Dollar {
 
     Dollar times(int multiplier) {
         return new Dollar(amount * multiplier);
+    }
+
+    public boolean equals(Object object) {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
     }
 }
